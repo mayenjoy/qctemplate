c39b33f9617a626ff41f586d672ac19a90a334ec
aurelienrb <aurelienrb@gmail.com>
2009-10-20 13:13:54 +0200

    Added example ForLoop

---
+++ examples/forloop/main.cpp
@@ -0,0 +1,30 @@
+#include "ForLoopTest.h"
+#include <QCoreApplication>
+#include <QTranslator>
+#include <qDebug>
+
+using namespace std;
+
+int main( int argc, char **argv )
+{
+    QCoreApplication app( argc, argv );
+    
+    ForLoopTest test;
+
+    qDebug() << "In English:";
+    qDebug() << test.toText(); 
+    qDebug() << test.toHtml();
+    
+    // load French translation
+    QTranslator translator;
+    translator.load( "forloop_fr");
+    app.installTranslator( &translator );
+         
+    // regenerate template data
+    test.reset(3);
+    
+    // display
+    qDebug() << "In French:";    
+    qDebug() << test.toText(); 
+    qDebug() << test.toHtml();
+}
